4b0905ea4380b895c3e6c89e9671ab08e4d45fb7
Beyza Keser <beyzaaakeser@gmail.com>
2024-09-30 16:43:57 +0300

    HOC modal & children added

--- src/pages/login/form.jsx
+++ src/pages/login/form.jsx
@@ -6,12 +6,15 @@ import React, { useState } from 'react';
 import { auth } from '../../firebase';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import MailModal from '../../components/modal/MailModal';
 
 const Form = () => {
   const [isSignUp, setIsSignUp] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
@@ -24,7 +27,9 @@ const Form = () => {
           navigate('/feed');
         })
         .catch((err) => {
-          toast.error('Hesabınızı oluştururken bir hata meydana geldi! ' +err.code );
+          toast.error(
+            'Hesabınızı oluştururken bir hata meydana geldi! ' + err.code
+          );
         });
     } else {
       signInWithEmailAndPassword(auth, email, password)
@@ -33,10 +38,14 @@ const Form = () => {
           navigate('/feed');
         })
         .catch((err) => {
-          toast.error('Hesaba giriş yapılırken bir hata meydana geldi! ' +err.code );
+          toast.error(
+            'Hesaba giriş yapılırken bir hata meydana geldi! ' + err.code
+          );
         });
     }
+    e.target.reset();
   };
+
   return (
     <>
       <form onSubmit={handleSubmit} className="flex flex-col">
@@ -45,6 +54,9 @@ const Form = () => {
 
         <label className="mt-4">Şifre</label>
         <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        <p onClick={() => setIsOpen(true)} className="text-sm text-gray-500 mt-2 text-end hover:text-gray-400 cursor-pointer ">
+          Şifremi Unuttum
+        </p>
 
         <button className="mt-10 bg-white text-black rounded-full p-1 font-bold transition hover:bg-gray-300">
           {isSignUp ? 'Kaydol' : 'Giriş Yap'}
@@ -63,6 +75,8 @@ const Form = () => {
           {isSignUp ? 'Giriş Yap' : 'Kaydol'}
         </span>
       </p>
+
+      <MailModal  isOpen={isOpen} close={() => setIsOpen(false)}/>
     </>
   );
 };
