55642b8a49bedd8b86f8c174b34b7eafa497d23b
Beyza Keser <beyzaaakeser@gmail.com>
2024-10-06 17:29:56 +0300

    project completed

--- src/components/post/Buttons.jsx
+++ src/components/post/Buttons.jsx
@@ -1,12 +1,26 @@
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 import React from 'react';
 import {
+  FaHeart,
   FaRegComment,
   FaRegHeart,
   FaRetweet,
   FaShareNodes,
 } from 'react-icons/fa6';
+import { auth, db } from '../../firebase';
 
 const Buttons = ({ tweet }) => {
+  const isLiked = tweet.likes.includes(auth.currentUser.uid);
+
+  const toggleLike = async () => {
+    const tweetRef = doc(db, 'tweets', tweet.id);
+
+    await updateDoc(tweetRef, {
+      likes: isLiked
+        ? arrayRemove(auth.currentUser.uid)
+        : arrayUnion(auth.currentUser.uid),
+    });
+  };
   return (
     <div className="flex justify-between items-center">
       <div
@@ -22,10 +36,12 @@ const Buttons = ({ tweet }) => {
         <FaRetweet />
       </div>
       <div
+        onClick={toggleLike}
         className="p-3 rounded-full cursor-pointer transition 
-      hover:bg-red-400/30"
+      hover:bg-red-400/30 flex items-center gap-1 "
       >
-        <FaRegHeart />
+        {isLiked ? <FaHeart className="text-red-500" /> : <FaRegHeart />}
+        {tweet.likes.length}
       </div>
       <div
         className="p-3 rounded-full cursor-pointer transition 
