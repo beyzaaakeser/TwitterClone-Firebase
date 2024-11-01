a1b989eb6a1bced34e8ac04164bc7e147a34db68
Beyza Keser <beyzaaakeser@gmail.com>
2024-11-01 13:36:10 +0300

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Feed from './pages/Feed';
-import Login from './pages/Login';
-import Protected from './components/protected';
+import Feed from './pages/Feed/index.jsx';
+import Login from './pages/Login/index.jsx';
+import Protected from './components/protected/index.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route element={<Protected/>}>
+        <Route element={<Protected />}>
           <Route path="/feed" element={<Feed />} />
           <Route path="/profil" element={<h1>Profil Sayfası</h1>} />
           <Route path="/ayar" element={<h1>Ayar Sayfası</h1>} />
